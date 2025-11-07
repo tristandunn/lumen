c7f0900766af371114ff45c00a47e74a801dcdd4
Tristan Dunn <hello@tristandunn.com>
2025-11-06 21:49:17 -0500

    Switch to a dispatch script for custom shortcuts.

--- config/hypr/scripts/firefox-reload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-active_class=$(hyprctl activewindow -j | jq -r '.class')
-address="0x$(hyprctl activewindow | head -n 1 | awk '{print $2}')"
-
-if [[ "$active_class" == "firefox" ]]; then
-  hyprctl dispatch sendshortcut CONTROL, R, address:"$address"
-fi
