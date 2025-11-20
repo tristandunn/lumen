de6a07f01eeff0c03a43c6b0efe180604638425c
Tristan Dunn <hello@tristandunn.com>
2025-11-19 20:40:30 -0500

    Don't use a deprecate vim method.

--- config/nvim/plug-ins/init.lua
+++ config/nvim/plug-ins/init.lua
@@ -2,7 +2,7 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 -- Install the package manager, if missing.
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
