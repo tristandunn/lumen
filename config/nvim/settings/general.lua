3a725056d535a495a638567fc4518b8dad30019f
Tristan Dunn <hello@tristandunn.com>
2026-01-19 01:09:10 -0500

    Disable unused language providers in Neovim for faster startup.

--- config/nvim/settings/general.lua
+++ config/nvim/settings/general.lua
@@ -4,6 +4,12 @@ local option = vim.opt
 
 global.is_posix = true -- Assume POSIX shell in vim.
 
+-- Disable unused language providers for faster startup.
+global.loaded_node_provider    = 0
+global.loaded_perl_provider    = 0
+global.loaded_python3_provider = 0
+global.loaded_ruby_provider    = 0
+
 
 option.exrc        = true  -- Enable local .nvim.lua files.
 option.mouse       = "a"   -- Enable mouse support in all modes.
